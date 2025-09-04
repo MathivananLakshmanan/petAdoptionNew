b45bc57aee8eb86882182b9e7ee8d7412102af0c
Mr_mathivanan <166470942+mathivanana123@users.noreply.github.com>
2025-09-04 21:26:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/petAdoptionNew/', // must match your GitHub repo name
-  build: {
-    outDir: 'dist' // Vite will auto-create this
-  }
+  base: '/petAdoptionNew/', 
 })
